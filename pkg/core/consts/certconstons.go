87f8fedb1c29bd6deb396956aeb5c176e1041646
shenshengkafei <wudiadi1111@gmail.com>
2018-05-07 16:19:53 -0700

    Updating credentials locations

--- pkg/core/consts/certconstons.go
+++ pkg/core/consts/certconstons.go
@@ -6,16 +6,16 @@ package consts
 
 const (
 	// SslCertKVBaseURI is the key vault base uri
-	SslCertKVBaseURI = "https://terraformkvwcus.vault.azure.net/"
+	SslCertKVBaseURI = "https://terraformkeyvaultwcus.vault.azure.net/"
 	// SslCertKVSecretName is the name of the secret
 	SslCertKVSecretName = "fullchain"
 	// SslCertKVSecretVersion is the version of the secret
-	SslCertKVSecretVersion = "6fbcc9bec49348bab3d173c655495012"
+	SslCertKVSecretVersion = "003247592dcd437b8fa446a4881772b6"
 
 	// SslPrivatekeyKVBaseURI is the key vault base uri
-	SslPrivatekeyKVBaseURI = "https://terraformkvwcus.vault.azure.net/"
+	SslPrivatekeyKVBaseURI = "https://terraformkeyvaultwcus.vault.azure.net/"
 	// SslPrivatekeyKVSecretName is the name of the secret
 	SslPrivatekeyKVSecretName = "privkey"
 	// SslPrivatekeyKVSecretVersion is the version of the secret
-	SslPrivatekeyKVSecretVersion = "679f1f7fd29345beb9f7946f161c32ae"
+	SslPrivatekeyKVSecretVersion = "85270ecf7e6d491eb4c06b89b034a102"
 )
--- pkg/core/consts/storageconstants.go
+++ pkg/core/consts/storageconstants.go
@@ -8,11 +8,11 @@ const (
 	// StorageDatabase is the name of data base
 	StorageDatabase = "terraformrpwcus"
 	// StoragePasswordKVBaseURI is the key vault base uri
-	StoragePasswordKVBaseURI = "https://terraformkvwcus.vault.azure.net/"
+	StoragePasswordKVBaseURI = "https://terraformkeyvaultwcus.vault.azure.net/"
 	// StoragePasswordKVSecretName is the name of storage password secret
 	StoragePasswordKVSecretName = "terraformstorageaccountkey"
 	// StoragePasswordKVSecretVersion is the version of storage password secret
-	StoragePasswordKVSecretVersion = "b917580b71714efe82f9777688ad2be6"
+	StoragePasswordKVSecretVersion = "9280f8c27e5a4bc2b29674a3b7e03009"
 	// ProviderRegistrationCollectionName is the provider collection name
 	ProviderRegistrationCollectionName = "providerRegistrations"
 	// ResourceCollectionName is the resouce collection name
