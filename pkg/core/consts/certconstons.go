de4d088e024900e0b2e0543b758ac6f5950a2204
shenshengkafei <wudiadi1111@gmail.com>
2018-06-04 16:38:22 -0700

    Renew certs

--- pkg/core/consts/certconstons.go
+++ pkg/core/consts/certconstons.go
@@ -10,12 +10,12 @@ const (
 	// SslCertKVSecretName is the name of the secret
 	SslCertKVSecretName = "fullchain"
 	// SslCertKVSecretVersion is the version of the secret
-	SslCertKVSecretVersion = "003247592dcd437b8fa446a4881772b6"
+	SslCertKVSecretVersion = "04a70b24963b40708805e237d8f214a8"
 
 	// SslPrivatekeyKVBaseURI is the key vault base uri
 	SslPrivatekeyKVBaseURI = "https://terraformkeyvaultwcus.vault.azure.net/"
 	// SslPrivatekeyKVSecretName is the name of the secret
 	SslPrivatekeyKVSecretName = "privkey"
 	// SslPrivatekeyKVSecretVersion is the version of the secret
-	SslPrivatekeyKVSecretVersion = "85270ecf7e6d491eb4c06b89b034a102"
+	SslPrivatekeyKVSecretVersion = "88ee30742342407ea846e6df2f764df0"
 )
